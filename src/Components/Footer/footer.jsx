eaf440a76a3f3a548c6b2bde9aae1a1220f1e683
Saksham <sakshamkumar1415@gmail.com>
2025-06-05 15:08:32 +0530

    Landing, Header, Footer done

---
+++ src/Components/Footer/footer.jsx
@@ -0,0 +1,116 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import Logo from '../../../public/KIK_logo-removebg.png'
+
+export default function Footer() {
+	return (
+		<div
+			id="footer"
+			className="bg-transparent flex flex-wrap justify-between items-center text-left mt-8 text-white font-[Poppins] border border-[#01011b] border-t-[#3f3e45]"
+		>
+			<div className="p-4 my-4 mx-10 sm:mx-4" id="footerItem1">
+				<div className="flex items-center mb-2">
+					<Image src={Logo} alt="KiK Logo" id="KiKLogo" className="w-[50px]" />
+					<h2 className="mt-2 ml-2 text-lg font-semibold">KodeinKGP</h2>
+				</div>
+				<div className="mt-4">
+					<p className="text-sm">
+						Indian Institute of Technology Kharagpur,
+						<br />
+						West Bengal - 721302
+					</p>
+				</div>
+				<div className="my-8 flex flex-wrap">
+					<a className="mr-4 my-2" target="_blank" href="https://www.facebook.com/kodeinkgp?mibextid=ZbWKwL" rel="noreferrer">
+						<ion-icon size="large" name="logo-facebook" className="text-white"></ion-icon>
+					</a>
+					<a className="mr-4 my-2" target="_blank" href="https://www.instagram.com/kodeinkgp_iitkgp?igsh=cmhyMXM3YTA5YzZm" rel="noreferrer">
+						<ion-icon size="large" name="logo-instagram" className="text-white"></ion-icon>
+					</a>
+					<a className="mr-4 my-2" target="_blank" href="https://www.linkedin.com/company/kodeinkgp/" rel="noreferrer">
+						<ion-icon size="large" name="logo-linkedin" className="text-white"></ion-icon>
+					</a>
+					<a className="mr-4 my-2" target="_blank" href="https://medium.com/@kodeinkgp" rel="noreferrer">
+						<ion-icon size="large" name="logo-medium" className="text-white"></ion-icon>
+					</a>
+					<a className="mr-4 my-2" target="_blank" href="https://chat.whatsapp.com/IFt69sSqZsu7FlRWl3EIbk" rel="noreferrer">
+						<ion-icon size="large" name="logo-whatsapp" className="text-white"></ion-icon>
+					</a>
+				</div>
+			</div>
+
+			<div className="p-4 my-4 mx-10 sm:mx-4" id="footerItem2">
+				<div className="font-bold text-lg pb-4" id="footerItemElement1">Blockchain</div>
+				<div className="my-2">
+					<a className="no-underline text-[wheat]" href="https://www.youtube.com/watch?v=gyMwXuJrbJQ" target="_blank" rel="noopener noreferrer">
+						freeCodeCamp
+					</a>
+				</div>
+				<div className="my-2">
+					<a className="no-underline text-[wheat]" href="https://www.youtube.com/@CodeEater21/videos" target="_blank" rel="noopener noreferrer">
+						Code Eater
+					</a>
+				</div>
+				<div className="my-2">
+					<a className="no-underline text-[wheat]" href="https://www.youtube.com/@WhiteboardCrypto" target="_blank" rel="noopener noreferrer">
+						Whiteboard Crypto
+					</a>
+				</div>
+				<div className="my-2">
+					<a className="no-underline text-[wheat]" href="https://www.youtube.com/@Finematics" target="_blank" rel="noopener noreferrer">
+						Finematics
+					</a>
+				</div>
+			</div>
+
+			<div className="p-4 my-4 mx-10 sm:mx-4" id="footerItem3">
+				<div className="font-bold text-lg pb-4" id="footerItemElement1">Machine Learning</div>
+				<div className="my-2">
+					<a className="no-underline text-[wheat]" href="https://www.youtube.com/@Deeplearningai" target="_blank" rel="noopener noreferrer">
+						DeepLearningAI
+					</a>
+				</div>
+				<div className="my-2">
+					<a className="no-underline text-[wheat]" href="https://www.youtube.com/@SirajRaval/about" target="_blank" rel="noopener noreferrer">
+						Siraj Raval
+					</a>
+				</div>
+				<div className="my-2">
+					<a className="no-underline text-[wheat]" href="https://www.youtube.com/@sentdex/playlists" target="_blank" rel="noopener noreferrer">
+						sentdex
+					</a>
+				</div>
+				<div className="my-2">
+					<a className="no-underline text-[wheat]" href="https://www.youtube.com/@coreyms/about" target="_blank" rel="noopener noreferrer">
+						Corey Schafer
+					</a>
+				</div>
+			</div>
+
+			<div className="p-4 my-4 mx-10 sm:mx-4" id="footerItem4">
+				<div className="font-bold text-lg pb-4" id="footerItemElement1">Web Development</div>
+				<div className="my-2">
+					<a className="no-underline text-[wheat]" href="https://www.youtube.com/playlist?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg" target="_blank" rel="noopener noreferrer">
+						CodeWithHarry
+					</a>
+				</div>
+				<div className="my-2">
+					<a className="no-underline text-[wheat]" href="https://www.youtube.com/playlist?list=PLfqMhTWNBTe3H6c9OGXb5_6wcc1Mca52n" target="_blank" rel="noopener noreferrer">
+						Apna College
+					</a>
+				</div>
+				<div className="my-2">
+					<a className="no-underline text-[wheat]" href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9ivBf_eKCPIAYXWzLlPAm6G" target="_blank" rel="noopener noreferrer">
+						net ninja
+					</a>
+				</div>
+				<div className="my-2">
+					<a className="no-underline text-[wheat]" href="https://www.youtube.com/playlist?list=PLillGF-RfqbZTASqIqdvm1R5mLrQq79CU" target="_blank" rel="noopener noreferrer">
+						Traversy Media
+					</a>
+				</div>
+			</div>
+		</div>
+	)
+}
---
+++ src/Components/Header/navbar.jsx
@@ -0,0 +1,120 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
+import Logo from '../../../public/KIK_logo-removebg.png'
+
+function Navbar() {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen)
+	}
+
+	return (
+		<div className={`sticky top-0 z-20 flex flex-wrap justify-between items-center px-4 md:px-8 bg-[rgba(1,1,27,0.6)] max-w-[99%] min-h-[5rem] ${isOpen ? 'flex-col h-screen bg-black w-full' : ''}`}>
+			
+			<div className={`flex flex-row items-center ml-[1.5vw] min-w-[20vw] ${isOpen ? 'hidden' : 'block'} md:flex`}>
+				<div className="flex items-center gap-[5px] text-[1.2rem]">
+					<Image src={Logo} alt="KodeinKGP Logo" className="w-[60px] h-[60px] p-[5px]" />
+					<h4 className="text-white font-bold">KodeinKGP</h4>
+				</div>
+			</div>
+
+			<button
+				id="hambutton"
+				className={`md:hidden flex flex-col justify-between w-[50px] h-[25px] bg-none border-none cursor-pointer ${isOpen ? 'fixed left-[82vw] top-[15px]' : ''}`}
+				onClick={toggleMenu}
+			>
+				<div className={`w-[30px] h-[3px] bg-[#eeeeee] transition-all duration-300 ease ${isOpen ? 'translate-x-[10px] translate-y-[18.2px] rotate-45 origin-center' : ''}`}></div>
+				<div className={`w-[30px] h-[3px] bg-[#eeeeee] transition-all duration-300 ease ${isOpen ? 'opacity-0' : ''}`}></div>
+				<div className={`w-[30px] h-[3px] bg-[#eeeeee] transition-all duration-300 ease ${isOpen ? 'translate-x-[10px] translate-y-[-2.2px] -rotate-45 origin-center' : ''}`}></div>
+			</button>
+
+			<div className={`min-w-[26vw] ml-auto ${isOpen ? 'flex justify-center items-center h-full w-full' : 'hidden'} md:block`}>
+
+				<div className={`flex ${isOpen ? 'flex-col' : 'flex-row'} items-center ${isOpen ? 'gap-4' : 'gap-[4rem]'}`}>
+					
+					<div className={`flex ${isOpen ? 'flex-col gap-4 items-start' : 'flex-row -mr-[20px]'}`}>
+						
+						<li className='cursor-pointer list-none' onClick={() => { if (isOpen) toggleMenu() }}>
+							<span className="relative flex flex-col items-center group text-[1.2rem] mx-[25px]">
+								<Link
+									className="no-underline text-white transition-colors duration-300 font-medium pb-[15px] tracking-wide bg-gradient-to-br"
+									href="/"
+								>
+									Home
+								</Link>
+								<span className="absolute bottom-3 left-0 h-[2px] w-full bg-[#11e3fb] scale-x-0 origin-left transition-transform duration-300 group-hover:scale-x-100"></span>
+							</span>
+						</li>
+						
+						<li className='cursor-pointer list-none' onClick={() => { if (isOpen) toggleMenu() }}>
+							<span className="relative flex flex-col items-center group text-[1.2rem] mx-[25px]">
+								<Link
+									className="no-underline text-white transition-colors duration-300 font-medium pb-[15px] tracking-wide bg-gradient-to-br"
+									href="/pds"
+								>
+									PDS_Bank
+								</Link>
+								<span className="absolute bottom-3 left-0 h-[2px] w-full bg-[#11e3fb] scale-x-0 origin-left transition-transform duration-300 group-hover:scale-x-100"></span>
+							</span>
+						</li>
+						
+						<li className='cursor-pointer list-none' onClick={() => { if (isOpen) toggleMenu() }}>
+							<span className="relative flex flex-col items-center group text-[1.2rem] mx-[25px]">
+								<Link
+									className="no-underline text-white transition-colors duration-300 font-medium pb-[15px] tracking-wide bg-gradient-to-br"
+									href="/articles"
+								>
+									Articles
+								</Link>
+								<span className="absolute bottom-3 left-0 h-[2px] w-full bg-[#11e3fb] scale-x-0 origin-left transition-transform duration-300 group-hover:scale-x-100"></span>
+							</span>
+						</li>
+						
+						<li className='cursor-pointer list-none' onClick={() => { if (isOpen) toggleMenu() }}>
+							<span className="relative flex flex-col items-center group text-[1.2rem] mx-[25px]">
+								<Link
+									className="no-underline text-white transition-colors duration-300 font-medium pb-[15px] tracking-wide bg-gradient-to-br"
+									href="/teams"
+								>
+									Teams
+								</Link>
+								<span className="absolute bottom-3 left-0 h-[2px] w-full bg-[#11e3fb] scale-x-0 origin-left transition-transform duration-300 group-hover:scale-x-100"></span>
+							</span>
+						</li>
+						
+						<li className='cursor-pointer list-none' onClick={() => { if (isOpen) toggleMenu() }}>
+							<span className="relative flex flex-col items-center group text-[1.2rem] mx-[25px]">
+								<Link
+									className="no-underline text-white transition-colors duration-300 font-medium pb-[15px] tracking-wide bg-gradient-to-br"
+									href="/events"
+								>
+									Events
+								</Link>
+								<span className="absolute bottom-3 left-0 h-[2px] w-full bg-[#11e3fb] scale-x-0 origin-left transition-transform duration-300 group-hover:scale-x-100"></span>
+							</span>
+						</li>
+						
+						<li className='cursor-pointer list-none' onClick={() => { if (isOpen) toggleMenu() }}>
+							<span className="relative flex flex-col items-center group text-[1.2rem] mx-[25px]">
+								<Link
+									className="no-underline text-white transition-colors duration-300 font-medium pb-[15px] tracking-wide bg-gradient-to-br"
+									href="/regform"
+								>
+									Registration Form
+								</Link>
+								<span className="absolute bottom-3 left-0 h-[2px] w-full bg-[#11e3fb] scale-x-0 origin-left transition-transform duration-300 group-hover:scale-x-100"></span>
+							</span>
+						</li>
+
+					</div>
+
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Navbar

d6576c838c2edd5a8574c3dc4e5b567152bb6d4a
Naitik Agarwal <naitikagar12@gmail.com>
2025-06-07 12:41:45 +0530

    added problemArena

--- src/app/deKodeX/page.jsx
+++ src/app/deKodeX/page.jsx
@@ -2,16 +2,17 @@
 
 import React from "react";
 import Leaderboard from "./Leaderboard";
+import ProblemArena from "./ProblemArena";
 
-export default function Layout({ children }) {
+export default function Layout() {
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col bg-[rgb(1,1,27)]">
         <div className="flex flex-1 w-full">
-            <div className="w-full md:w-[70%] bg-blue-700 p-6 text-white">
-                {children || <p>Questions</p>}
+            <div className="w-full md:w-[70%] bg-700 p-6 text-white">
+                <ProblemArena/>
             </div>
 
-            <div className="hidden md:block w-[30%] bg-blue-400 p-6 text-white">
+            <div className="hidden md:block w-[30%] bg-400 p-6 text-white">
                 <Leaderboard />
             </div>
         </div>

0e4bcb3cd75fe2662ea2fd16edf42b2b80c1ecc6
Aashrith Vellampalli <aashrith@Aashriths-MacBook-Pro.local>
2025-12-26 00:51:51 +0530

    new our works cards

--- src/Components/utils/BubbleText.jsx
+++ src/Components/utils/BubbleText.jsx
@@ -14,7 +14,11 @@ function BubbleText() {
         return () => clearInterval(timer);
     }, [idx]);
     return (
-        <div className='text-lg text-white'>{text}
+        <div className='flex flex-col gap-3 pb-8 mb-4'>
+            <div className='text-lg text-white'>{text}</div>
+            <p className='text-xs text-gray-400 max-w-md leading-relaxed mb-6'>
+                "The Web as I envisaged it, we have not seen it yet. The future is still so much bigger than the past"
+            </p>
         </div>
     )
 }

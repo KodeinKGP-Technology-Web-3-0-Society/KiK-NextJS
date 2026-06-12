f31bad662211a82a4870265ea841b255e1842f6c
Exporise <exporiseofficial@gmail.com>
2026-06-12 18:57:34 +0530

    Add google signin button

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -2,6 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+//Signin through Google
+import { GoogleAuthProvider } from "firebase/auth";
+export const googleProvider = new GoogleAuthProvider();
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
